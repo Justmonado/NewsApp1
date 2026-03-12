a7dbd71e3090aa17f3992aa982d3c3442408ff17
Alfonso Soots <alfonso_soots@hotmail.com>
2026-03-11 21:59:49 -0600

    News

--- app/src/main/java/com/pajasoft/newsapp/MainActivity.kt
+++ app/src/main/java/com/pajasoft/newsapp/MainActivity.kt
@@ -9,15 +9,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pajasoft.newsapp.components.Buscador
+import com.pajasoft.newsapp.components.Cardimg
+import com.pajasoft.newsapp.components.NewsCard
 import com.pajasoft.newsapp.components.Noticias
+import com.pajasoft.newsapp.models.newsList
 import com.pajasoft.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,6 +57,33 @@ fun NewsApp(innerPadding: PaddingValues){
     ) {
         Buscador()
         Noticias()
+
+        Text(
+            text = "Ultimas Noticias",
+            fontWeight = FontWeight.Bold,
+            fontSize = 25.sp,
+            modifier = Modifier
+                .padding(top = 15.dp, bottom = 10.dp)
+        )
+        LazyRow {
+            items(newsList){noticias ->
+                NewsCard(noticia = noticias)
+            }
+        }
+        Text(
+            text = "Alrededor del Mundo",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(top = 15.dp, bottom = 10.dp)
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2)
+        ) {
+            items(newsList){noticias ->
+                Cardimg(noticia = noticias)
+            }
+        }
     }
 }
 
